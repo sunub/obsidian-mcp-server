b2a60ee83f51062b13246454249142afe727b170
sunub <bsc5672@gmail.com>
2025-09-12 18:08:29 +0900

    feat : window, linux 경로를 구분하고 linux 경로로 통합하는 유틸 함수 작성

---
+++ src/utils/parseVaultPath.ts
@@ -0,0 +1,20 @@
+import os from 'os';
+
+export const VAULT_DIR_PATH = process.env.VAULT_DIR_PATH || '';
+
+function isWindowsPath(path: string): boolean {
+  return os.platform() === 'win32' && /^[a-zA-Z]:\\/.test(path);
+}
+
+function parseWindosPathToLinux(path: string): string {
+  if (!isWindowsPath(path)) {
+    throw new Error('Not a valid Windows path');
+  }
+
+  const driveLetter = path[0].toLowerCase();
+  const pathWithoutDrive = path.slice(2).replace(/\\/g, '/');
+  return `/mnt/${driveLetter}/${pathWithoutDrive}`;
+}
+
+export const getParsedVaultPath = () =>
+  isWindowsPath(VAULT_DIR_PATH) ? parseWindosPathToLinux(VAULT_DIR_PATH) : VAULT_DIR_PATH;
