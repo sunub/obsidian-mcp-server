273318ccd1e0c30ce5d0dac7b2052493ca5bbac4
sunub <bsc5672@gmail.com>
2025-09-12 18:11:56 +0900

    feat(tool) : 작성한 도구들을 통합하는 배럴 파일 작성

---
+++ src/tools/index.ts
@@ -0,0 +1,11 @@
+import ObsidianVaultTool from './obsidian_vault/index.js';
+import GenerateObsidianVaultPropertiesTool from './generate_obsidian_property/index.js';
+import WriteObsidianPropertyTool from './write_obsidian_property/index.js';
+import CreateDocumentWithPropertyTool from './create_document_with_properties/index.js';
+
+export default {
+  ObsidianVaultTool,
+  GenerateObsidianVaultPropertiesTool,
+  WriteObsidianPropertyTool,
+  CreateDocumentWithPropertyTool
+}
