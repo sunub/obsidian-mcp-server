912d53339c3dd218cc4af449389fed360c31521b
sunub <bsc5672@gmail.com>
2025-09-18 22:50:27 +0900

    fix : Tool에 새롭게 작성한 도구를 등록하지 않아 발생하는 오류 수정

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -2,10 +2,12 @@ import ObsidianVaultTool from './vault/index.js';
 import GenerateObsidianVaultPropertiesTool from './generate_property/index.js';
 import WriteObsidianPropertyTool from './write_property/index.js';
 import CreateDocumentWithPropertyTool from './create_document_with_properties/index.js';
+import OrganizeAttachmentsTool from './organize_attachments/index.js';
 
 export default {
   ObsidianVaultTool,
   GenerateObsidianVaultPropertiesTool,
   WriteObsidianPropertyTool,
-  CreateDocumentWithPropertyTool
+  CreateDocumentWithPropertyTool,
+  OrganizeAttachmentsTool,
 }
