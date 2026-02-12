9ed20e291838c198ef0992cad445f9a8b4afa1c0
sunub <bsc5672@gmail.com>
2026-02-12 14:26:34 +0900

    refactor : 사용하지 않는 불필요한 파일 제거

--- src/utils/isIterable.ts
+++
@@ -1,7 +0,0 @@
-export function isIterable(obj: unknown): obj is Iterable<unknown> {
-	return (
-		obj != null &&
-		typeof (obj as { [Symbol.iterator]?: unknown })[Symbol.iterator] ===
-			"function"
-	);
-}
